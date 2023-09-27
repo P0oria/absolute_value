7e1860758df8370ca7b55dacbeb52867840135fc
Pooria <pooria.tayaran@gmail.com>
2023-09-27 16:15:43 -0400

    Added comments.

--- absolute_value/main.cpp
+++ absolute_value/main.cpp
@@ -2,6 +2,7 @@
 
 #include <cstdio>
 
+// Function to calculate the absolute value of a number.
 int absolute_value(int x) {
     if (x > 0 || x == 0) {
         return x;
@@ -10,6 +11,7 @@ int absolute_value(int x) {
     }
 }
 
+// The main function of the program.
 int main() {
   int my_num = -5;
   printf("The absolute value of %d is %d.\n", my_num,
